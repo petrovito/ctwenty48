52cd2543e620dd5884461d4e6f78f6d2f86fe269
PZ <zarandy.peter@gmail.com>
2022-10-28 17:28:55 +0200

    C20-12 add control panels

--- include/gui/widgets.hh
+++ include/gui/widgets.hh
@@ -3,9 +3,11 @@
 #include <array>
 #include <nana/gui.hpp>
 #include <nana/gui/place.hpp>
+#include <nana/gui/widgets/button.hpp>
+#include <nana/gui/widgets/group.hpp>
 #include <nana/gui/widgets/label.hpp>
-
 #include <nana/gui/widgets/panel.hpp>
+#include <nana/gui/widgets/tabbar.hpp>
 #include <types.hh>
 #include <vector>
 
@@ -19,8 +21,44 @@ namespace c20::gui {
 			nana::place place;
 			std::unique_ptr<nana::label> squares[TABLE_SIZE][TABLE_SIZE];
 		public:
-			TablePanel(nana::form&);
+			TablePanel(nana::window);
 			void set_position(commons::Position&);
+	};
+
+
+	class MainTab : public nana::panel<false>
+	{
+		private:
+			nana::place place;
+
+			nana::group btn_group;
+
+			nana::button start_btn;
+		public:
+			MainTab(nana::window);
+	};
+
+	class HistoryTab : public nana::panel<false>
+	{
+		private:
+			nana::place place;
+		public:
+			HistoryTab(nana::window);
+	};
+
+
+	class ControlPanel : public nana::panel<false>
+	{
+		private:
+			nana::place place;
+			nana::tabbar<std::string> tab_bar;
+
+			MainTab main_tab;
+			HistoryTab history_tab;
+
+		public:
+			ControlPanel(nana::window);
+
 	};
 
 }
--- src/gui/test.cc
+++ src/gui/test.cc
@@ -1,4 +1,5 @@
 #include "types.hh"
+#include <iostream>
 #include <nana/basic_types.hpp>
 #include <nana/gui.hpp>                  // always include this
 #include <nana/gui/widgets/button.hpp>
@@ -38,7 +39,7 @@ namespace c20::gui {
 		0x000000,
 	};
 
-	TablePanel::TablePanel(nana::form& fm) :
+	TablePanel::TablePanel(nana::window fm) :
 		nana::panel<false>(fm),
 		place(*this)
 	{
@@ -67,6 +68,56 @@ namespace c20::gui {
 			}
 	}
 
+	ControlPanel::ControlPanel(nana::window fm) :
+		nana::panel<false>(fm),
+		place(*this),
+		tab_bar(*this),
+		main_tab(*this),
+		history_tab(*this)
+	{
+		place.div(R"(vertical 
+			<tab weight=20>
+			<tab_frame>
+		)");
+
+		place["tab"] << tab_bar;
+		place["tab_frame"].fasten(main_tab)
+			.fasten(history_tab);
+
+		tab_bar.append("main", main_tab)
+			.append("history", history_tab);
+		tab_bar.activated(0);
+
+	}
+
+	MainTab::MainTab(nana::window fm) : 
+		nana::panel<false>(fm),
+		place(*this),
+		btn_group(*this, "Game"),
+		start_btn(btn_group, "Start game")
+	{
+		place.div(R"( 
+			vertical 
+			<all   weight=160 gap=3 margin=25>
+			<rest>
+		 )");
+		place["all"] << btn_group;
+
+		btn_group.div(R"(
+			<weight=10>
+		    <buttons weight=125 gap=5 margin=20>
+			<weight=10>
+		)");
+		btn_group["buttons"] << start_btn;
+
+		start_btn.events().click([] (auto& var) {std::cout<<"CLICKED"<<std::endl;});
+	}
+
+	HistoryTab::HistoryTab(nana::window fm) :
+		nana::panel<false>(fm),
+		place(*this)
+	{}
+
 
 }
 
@@ -77,46 +128,21 @@ int main()
 {
 	using namespace nana;
 	form   fm ;                             // Our main window
-	fm.caption("My first NANA demo");       // (with this title)
-	fm.size({500,400});
+	fm.caption("c2048");       // (with this title)
+	fm.size({900,400});
 	place  fm_place{fm};                    // have automatic layout
-	bool   really_quick{false};             // and a default behavior
-	label  hello{fm,"Hello World"};         // We put a label on our window
-	button btn  {fm,"Quit"};                // and a button
-	btn.tooltip( "I will ask first");       // that show a tip upon mouse hover
-	group  act  {fm, "Actions"};            // Add a group of "options"
-	act.add_option("Quick quickly")         // and two options that control quick behavior
-		.events().click([&]()   {   really_quick=true;
-				btn.tooltip("Quick quickly");       });
-	act.add_option("Ask first")
-		.events().click([&]()   {   really_quick=false;
-				btn.tooltip("I will ask first");    });
-	btn.events().click([&]()               // now the button know how to respond
-			{
-			if (false)     // not really quick !
-			{
-			msgbox m(fm,"Our demo", msgbox::yes_no) ;
-			m.icon(m.icon_question);
-			m << "Are you sure you want to quick?";
-			auto response= m();
-			if (response != m.pick_yes) return;   // return to the demo
-			}
-			API::exit();           // or really quick
-			});
-	act.radio_mode(true);                   // Set "radio mode" (only one option selected)
-											// let divide fm into fields to holds the other controls.
-											// for example, let split fm into two fields separated by a movable vertical barre.
-	fm_place.div("horizontal <table margin=3 weight=400><actions>");
-	fm_place["actions"  ]<< hello << btn;        // and place the controls there
 
+	fm_place.div("horizontal <table margin=3 weight=400><actions>");
 
 	c20::gui::TablePanel bar(fm);
 	auto pos = c20::commons::Position::from_str("1111|2222|3333|4444");
 	bar.set_position(pos);
 
-	fm_place["table"] << bar;
+	c20::gui::ControlPanel control(fm);
 
-	/* fm_place[name.c_str()]<< act ; */
+	fm_place["table"] << bar;
+	fm_place["actions"] << control;
+
 	fm_place.collocate();                      // and collocate all in place
 	fm.show();
 	exec();
